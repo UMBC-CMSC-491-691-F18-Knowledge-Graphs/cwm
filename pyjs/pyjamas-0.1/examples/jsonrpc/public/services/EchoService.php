b2d8b86b4448b2bbc69967e0e124e2c231a30004
Tim Finin <tfinin@gmail.com>
2018-10-02 20:42:33 -0400

    .

---
+++ pyjs/pyjamas-0.1/examples/jsonrpc/public/services/EchoService.php
@@ -0,0 +1,9 @@
+<?php
+include_once("phpolait/phpolait.php");
+
+class EchoService {
+    function myecho($msg) { return $msg; }
+}
+
+$server = new JSONRpcServer(new EchoService(), array("echo"=>"myecho"));
+?>
